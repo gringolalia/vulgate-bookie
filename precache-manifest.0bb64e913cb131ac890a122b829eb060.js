1a0e6633765514ca91c002a26b13dbdb07963ed7
Colin Brayton <gringolalia@gmail.com>
2020-05-12 15:51:56 -0300

    deploy

---
+++ precache-manifest.0bb64e913cb131ac890a122b829eb060.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafcbd29263ab5716570",
+    "url": "/vulgate-bookie/css/app.90198406.css"
+  },
+  {
+    "revision": "107f2962794557dd36c4",
+    "url": "/vulgate-bookie/css/chunk-vendors.2d15fb80.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vulgate-bookie/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dd02b1ddf377ba7fb0cf1c2859ad6a1d",
+    "url": "/vulgate-bookie/index.html"
+  },
+  {
+    "revision": "715e88fbe1fdaa3fe67c",
+    "url": "/vulgate-bookie/js/about.8f2eb496.js"
+  },
+  {
+    "revision": "aafcbd29263ab5716570",
+    "url": "/vulgate-bookie/js/app.8495b6a3.js"
+  },
+  {
+    "revision": "107f2962794557dd36c4",
+    "url": "/vulgate-bookie/js/chunk-vendors.34680438.js"
+  },
+  {
+    "revision": "74ab88a679911899b6a4cb898eca675e",
+    "url": "/vulgate-bookie/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vulgate-bookie/robots.txt"
+  }
+]);
